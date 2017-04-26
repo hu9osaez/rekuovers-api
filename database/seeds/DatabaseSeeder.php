dd3619b946d0c71c4d00a97cb907aaf4bba43763
Hugo Saez <hu.saezs@gmail.com>
2017-04-26 01:16:23 -0300

    260420170116

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        Schema::disableForeignKeyConstraints();
 
         $this->call(ArtistTableSeeder::class);
         $this->call(OriginalSongsTableSeeder::class);
         $this->call(SongsTableSeeder::class);
         $this->call(LikesTableSeeder::class);
+
+        Model::reguard();
+        Schema::enableForeignKeyConstraints();
     }
 }

24b5d0a63cd238c638893d54db4186a19749ab27
Hugo Saez <hu.saezs@gmail.com>
2017-05-03 21:29:52 -0300

    030520172129

--- app/Transformers/SongTransformer.php
+++ app/Transformers/SongTransformer.php
@@ -10,11 +10,11 @@ class SongTransformer extends TransformerAbstract
         $formattedSong = [
             'id' => (int) $song->id,
             'original_song_id' => $song->original_song_id,
+            'artist_id' => $song->originalSong->artist->id,
             'type' => $song->type,
             'youtube_id' => $song->youtube_id,
             'likes_count' => $song->likes->count(),
-            'created_at' => $song->created_at->toDateTimeString(),
-            'updated_at' => $song->updated_at->toDateTimeString()
+            'created_at' => $song->created_at->toDateTimeString()
         ];
 
         return $formattedSong;
